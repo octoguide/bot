e5ca9202b863b1b92debd933d55cd59780985a4d
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-23 20:08:10 -0400

    wip: test

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,8 +1,10 @@
+import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 import { runOctoGuideAction } from "./runOctoGuideAction.js";
 
 console.log("Running src/action/index");
-console.log(process.env);
+console.log("env:", process.env);
+console.log("context:", github.context);
 
 await runOctoGuideAction(github.context);
