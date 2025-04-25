f3abb3ff9cfeb3da4bcdb83e86cf19ba2ec93051
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-25 08:35:59 -0400

    Remove unnecessary core-js

--- src/action/groupBy.ts
+++ src/action/groupBy.ts
@@ -1,6 +1,3 @@
-// Object.groupBy is not a function
-// https://github.com/actions/runner/issues/3600
-import "core-js/proposals/array-grouping-v2.js";
 import _ from "lodash";
 
 // I promise this works.
