00c6150b908f2023a6aa4888867877105b71ba12
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-28 08:38:58 -0400

    Corrected createActor tests

--- src/actors/createActor.test.ts
+++ src/actors/createActor.test.ts
@@ -11,62 +11,58 @@ const mockOctokit = {} as Octokit;
 
 describe(createActor, () => {
 	it("creates a DiscussionCommentActor when given a discussion comment URL", () => {
-		describe(createActor, () => {
-			it("creates a DiscussionCommentActor when given a discussion comment URL", () => {
-				const url = "discussions/123#discussioncomment-456";
+		const url = "discussions/123#discussioncomment-456";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeInstanceOf(DiscussionCommentActor);
-			});
+		expect(actor).toBeInstanceOf(DiscussionCommentActor);
+	});
 
-			it("creates a DiscussionActor when given a discussion URL", () => {
-				const url = "discussions/123";
+	it("creates a DiscussionActor when given a discussion URL", () => {
+		const url = "discussions/123";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeInstanceOf(DiscussionActor);
-			});
+		expect(actor).toBeInstanceOf(DiscussionActor);
+	});
 
-			it("creates an IssueLikeCommentActor when given an issue comment URL", () => {
-				const url = "issues/123#issuecomment-456";
+	it("creates an IssueLikeCommentActor when given an issue comment URL", () => {
+		const url = "issues/123#issuecomment-456";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeInstanceOf(IssueLikeCommentActor);
-			});
+		expect(actor).toBeInstanceOf(IssueLikeCommentActor);
+	});
 
-			it("creates an IssueLikeActor when given an issue URL", () => {
-				const url = "issues/123";
+	it("creates an IssueLikeActor when given an issue URL", () => {
+		const url = "issues/123";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeInstanceOf(IssueLikeActor);
-			});
+		expect(actor).toBeInstanceOf(IssueLikeActor);
+	});
 
-			it("creates an IssueLikeCommentActor when given a pull request comment URL", () => {
-				const url = "pull/123#issuecomment-456";
+	it("creates an IssueLikeCommentActor when given a pull request comment URL", () => {
+		const url = "pull/123#issuecomment-456";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeInstanceOf(IssueLikeCommentActor);
-			});
+		expect(actor).toBeInstanceOf(IssueLikeCommentActor);
+	});
 
-			it("creates an IssueLikeActor when given a pull request URL", () => {
-				const url = "pull/123";
+	it("creates an IssueLikeActor when given a pull request URL", () => {
+		const url = "pull/123";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeInstanceOf(IssueLikeActor);
-			});
+		expect(actor).toBeInstanceOf(IssueLikeActor);
+	});
 
-			it("returns undefined when given an invalid URL", () => {
-				const url = "invalid/url";
+	it("returns no actor when given an invalid URL", () => {
+		const url = "invalid/url";
 
-				const actor = createActor(mockOctokit, url);
+		const { actor } = createActor(mockOctokit, url);
 
-				expect(actor).toBeUndefined();
-			});
-		});
+		expect(actor).toBeUndefined();
 	});
 });
