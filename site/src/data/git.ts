1525b79b39ac9cf0ff95ed5bcc51b4fc166824d0
Josh Goldberg <git@joshuakgoldberg.com>
2025-05-01 08:25:17 -0400

    testing: pwd

--- site/src/data/git.ts
+++ site/src/data/git.ts
@@ -2,6 +2,8 @@ import cp from "node:child_process";
 
 import { version } from "./package.js";
 
+console.log("pwd:", cp.execSync("pwd").toString().trim());
+
 export const latestCommit = cp
 	.execSync(`git rev-list -n 1 ${version}`)
 	.toString()
