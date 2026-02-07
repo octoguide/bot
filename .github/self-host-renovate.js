eddbc3fb1faccb03ab5b503d22c883ef14912d6c
cylewaitforit <cyle@cylewaitfor.it>
2026-02-07 09:15:09 -0600

    fix: typo

--- .github/self-host-renovate.js
+++ .github/self-host-renovate.js
@@ -1,6 +1,6 @@
 // Only self hosted configuration should be in this file.
 // Renovate configuration should be placed in .github/renovate.json
 // See https://docs.renovatebot.com/self-hosted-configuration/
-modules.exports = {
+module.exports = {
 	repositories: ["octoguide/bot"],
 };
