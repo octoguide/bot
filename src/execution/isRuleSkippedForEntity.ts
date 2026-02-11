f1cf64db615b3685f89b3a53bbb4767865695ceb
Josh Goldberg <git@joshuakgoldberg.com>
2026-02-11 14:08:51 -0500

    Progress: includeAssociations

--- src/execution/isRuleSkippedForEntity.ts
+++ src/execution/isRuleSkippedForEntity.ts
@@ -1,12 +1,19 @@
+import { isEntityAssociationIncluded } from "../action/isEntityAssociationIncluded";
 import { isEntityFromBot } from "../action/isEntityFromBot";
 import { Entity } from "../types/entities";
 import { RuleOptions } from "../types/rules";
 
 export function isRuleSkippedForEntity(entity: Entity, options: RuleOptions) {
-	// TODO: include-associations?
+	const includeAssociations = options["include-associations"];
+	if (
+		includeAssociations &&
+		!isEntityAssociationIncluded(entity, includeAssociations)
+	) {
+		return true;
+	}
 
 	const includeBots = options["include-bots"];
-	if (isEntityFromBot(entity) && !includeBots) {
+	if (!includeBots && isEntityFromBot(entity)) {
 		return true;
 	}
 
